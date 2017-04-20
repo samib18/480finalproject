f638653b6d83d81f800cce09f1a9f2ab187b370e
samibialozynski <samibialozynski@10.11.10.69>
2017-04-20 09:35:26 -0400

    URL

--- Schedule.java
+++ Schedule.java
@@ -1,1 +1,16 @@
+import java.net.*;
+import java.io.*;
 
+public class Schedule {
+    public static void main(String[] args) throws Exception {
+
+        URL scheduleReader = new URL("https://admin.washcoll.edu/schedules/17SPschedules.txt");
+        BufferedReader in = new BufferedReader(
+        new InputStreamReader(scheduleReader.openStream()));
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+        in.close();
+    }
+}
