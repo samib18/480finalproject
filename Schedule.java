70d140e14946f293e5d46e94b4da3a48ce99062a
samibialozynski <samibialozynski@10.11.10.69>
2017-04-25 16:51:45 -0400

    4:51 Sami Jsoup Edits

--- Schedule.java
+++ Schedule.java
@@ -1,4 +1,13 @@
 import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import java.io.*;
 
 public class Schedule {
@@ -7,10 +16,28 @@ public class Schedule {
         URL scheduleReader = new URL("https://admin.washcoll.edu/schedules/17SPschedules.txt");
         BufferedReader in = new BufferedReader(
         new InputStreamReader(scheduleReader.openStream()));
-
-        String inputLine;
-        while ((inputLine = in.readLine()) != null)
-            System.out.println(inputLine);
+       
+        Document doc = Jsoup.connect("https://admin.washcoll.edu/schedules/17SPschedules.txt").get();
+        //doc.outputSettings().prettyPrint(false);
+        
+        Element link = doc.body().wrap("A");
+        String text = doc.text();
+        
+        String inputLine = doc.body().text();
+        String linkHref = link.attr("HREF");
+        String linkText = link.text();
+        
+        String linkOuterH = link.outerHtml();
+        String linkInnerH = link.html();
+        
+        List<String> list = new ArrayList<>();
+        
+        	for(Element element : doc.select("A"))
+        	{
+        		list.add(element.nextSibling().toString());
+        	}
+        	
+        	System.out.println(text + list);
         in.close();
         //hello
     }
