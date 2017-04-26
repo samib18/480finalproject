ff9cb535802e2245b1e4778f1d402869f48375f5
samibialozynski <samibialozynski@10.11.10.69>
2017-04-26 08:41:27 -0400

    new edits 4/26 8:41 Sami

--- Schedule.java
+++ Schedule.java
@@ -3,9 +3,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-import org.jsoup.Jsoup;
+import org.jsoup.*;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.safety.Whitelist;
 import org.jsoup.select.Elements;
 
 import java.io.*;
@@ -13,32 +14,19 @@ import java.io.*;
 public class Schedule {
     public static void main(String[] args) throws Exception {
 
-        URL scheduleReader = new URL("https://admin.washcoll.edu/schedules/17SPschedules.txt");
+        URL scheduleReader = new URL("https://admin.washcoll.edu/schedules/17SPschedules.html");
         BufferedReader in = new BufferedReader(
         new InputStreamReader(scheduleReader.openStream()));
        
         Document doc = Jsoup.connect("https://admin.washcoll.edu/schedules/17SPschedules.txt").get();
-        //doc.outputSettings().prettyPrint(false);
         
-        Element link = doc.body().wrap("A");
-        String text = doc.text();
+        Elements link = doc.body().select("*");
         
-        String inputLine = doc.body().text();
-        String linkHref = link.attr("HREF");
-        String linkText = link.text();
-        
-        String linkOuterH = link.outerHtml();
-        String linkInnerH = link.html();
-        
-        List<String> list = new ArrayList<>();
-        
-        	for(Element element : doc.select("A"))
+        	for(Element element : link)
         	{
-        		list.add(element.nextSibling().toString());
+        			System.out.println(element.ownText());
         	}
         	
-        	System.out.println(text + list);
         in.close();
-        //hello
     }
 }
