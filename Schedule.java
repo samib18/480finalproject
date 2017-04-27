6d7dc2f57eb48bf0f002d947b6e6a7c50fdf6fe8
samibialozynski <samibialozynski@10.11.10.69>
2017-04-27 12:34:32 -0400

    Edits 12:34 April 27 Sami

--- Schedule.java
+++ Schedule.java
@@ -10,14 +10,16 @@ public class Schedule {
     	String currentYear;
     	String currentSemester;
     	String classDescriptors;
+    	String[] classes;
     	
     	System.out.println("Enter in the Class Descriptors you want to Take: ");
-    	classDescriptors = input.next();
+    	classDescriptors = input.nextLine();
+    	classes = classDescriptors.split(" ");
     	System.out.println("Enter in Current Year (Ex: 2017 = 17): ");
     	currentYear = input.next();
     	System.out.println("Enter in FA for Fall Semester or SP for Spring Semester: ");
     	currentSemester = input.next();
-    	
+    
         URL scheduleReader = new URL("https://admin.washcoll.edu/schedules/" + currentYear + currentSemester + "schedules.txt");
         BufferedReader in = new BufferedReader(
         new InputStreamReader(scheduleReader.openStream()));
@@ -34,32 +36,36 @@ public class Schedule {
         	
         	Boolean grw = line.contains("GRW");
         	
-        	if (line.contains(classDescriptors)){
-        		if (quitPosition >= 0 ){
-            		System.out.println(line.substring(89, 99) + line.substring(104, quitPosition));
-            	}else if(quitPosition2 >= 0){
-            		System.out.println(line.substring(89, 99) + line.substring(104, quitPosition2));
-            	}else if(quitPosition3 >= 0){
-            		if(grw == true){
-            			System.out.println(line.substring(121, 131) + line.substring(136, quitPosition3));
-            		}else{
-            			System.out.println(line.substring(89, 99) + line.substring(104, quitPosition3));
-            		}
-            	}else if(quitPosition4 >= 0){
-            		System.out.println(line.substring(89, 99) + line.substring(104, quitPosition4));
-            	}else if(quitPosition5 >= 0){
-            		System.out.println(line.substring(89, 99) + line.substring(104, quitPosition5));
-            	}else if(quitPosition6 >= 0){
-            		System.out.println(line.substring(89, 99) + line.substring(104, quitPosition6));
-            	}else if(quitPosition7 >= 0){
-            		if(grw == true){
-            			System.out.println(line.substring(121, 131) + line.substring(136, quitPosition7));
-            		}else{
-            			System.out.println(line.substring(89, 99) + line.substring(104, quitPosition7));
-            		}
-            	}else{
-            		System.out.println(line);
-            	}
+        	int classesSize = classes.length;
+        	
+        	for (int i = 0; i < classesSize; i++){
+        		if (line.contains(classes[i])){
+        			if (quitPosition >= 0 ){
+        				System.out.println(line.substring(89, 99) + line.substring(104, quitPosition));
+        			}else if(quitPosition2 >= 0){
+        				System.out.println(line.substring(89, 99) + line.substring(104, quitPosition2));
+        			}else if(quitPosition3 >= 0){
+        				if(grw == true){
+        					System.out.println(line.substring(121, 131) + line.substring(136, quitPosition3));
+        				}else{
+        					System.out.println(line.substring(89, 99) + line.substring(104, quitPosition3));
+        				}
+        			}else if(quitPosition4 >= 0){
+        				System.out.println(line.substring(89, 99) + line.substring(104, quitPosition4));
+        			}else if(quitPosition5 >= 0){
+        				System.out.println(line.substring(89, 99) + line.substring(104, quitPosition5));
+        			}else if(quitPosition6 >= 0){
+        				System.out.println(line.substring(89, 99) + line.substring(104, quitPosition6));
+        			}else if(quitPosition7 >= 0){
+        				if(grw == true){
+        					System.out.println(line.substring(121, 131) + line.substring(136, quitPosition7));
+        				}else{
+        					System.out.println(line.substring(89, 99) + line.substring(104, quitPosition7));
+        				}
+        			}else{
+        				System.out.println(line);
+        			}
+        		}
         	}
         }      
     }
